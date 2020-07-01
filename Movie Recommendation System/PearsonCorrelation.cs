39beff61d16f168d2d1e2ae2cc1c7eab8d555306
Richmond97 <36780094+Richmond97@users.noreply.github.com>
2020-07-01 18:55:42 +0100

    New Class (Pearson Correlation) made, with structure started. not sure if right but its something

---
+++ Movie Recommendation System/PearsonCorrelation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie_Recommendation_System
+{
+    public class PearsonCorrelation
+    {
+        private ArrayList collection = new ArrayList();
+        //what the algorithm does
+        // finds the reviews that reviewed both products. 
+        //it then computes the sums of the ratings and the squared sums of the ratings for the two products
+        //and computes the sum of the reviews of the products.
+        //Finally it uses these results to compute the PC score.
+
+        public void CalculatePearsonScore()
+        {
+            GetCommonReviews();
+            SumOfPref();
+            SumOfSquared();
+            SumOfProduct();
+            SortInDescending();
+            PearsonScore();
+
+        }
+
+        //Collect list of products which have reviews in common
+        public void GetCommonReviews()
+        {
+            foreach (var item in collection)
+	        {
+                
+	        }
+        }
+
+       //Get the sum of all the preferences
+       public void SumOfPref()
+       {
+
+       }
+
+       //get sum of squared preferences
+       public void SumOfSquared()
+        {
+
+        }
+
+       public void SumOfProduct()
+       {
+
+       }
+
+       public void PearsonScore()
+        {
+
+        }
+
+        public void SortInDescending()
+        {
+
+        }
+
+        
+    }
+}
